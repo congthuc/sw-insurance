33eea630a5f762fe7719802cd313b7cefd88644b
congthucphan <congthuc.uit@gmail.com>
2025-10-16 17:02:55 +0700

    Initial commit

---
+++ src/main/java/com/insurance/pct/sw_insurance/SwInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.pct.sw_insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwInsuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insurance/pct/sw_insurance/SwInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.pct.sw_insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
